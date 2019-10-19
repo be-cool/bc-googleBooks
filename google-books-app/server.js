79a57980646ff9aa7d772612600cb2f216ad4ea8
Ben Cooley <bcooley58@gmail.com>
2019-10-19 12:51:00 -0400

    move mongoose connection

--- google-books-app/server.js
+++ google-books-app/server.js
@@ -7,15 +7,6 @@ const app = express();
 dotenv.config({ path: "./config.env" });
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
-app.use(routes);
-
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch",
   {
@@ -25,6 +16,16 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
 
 // Start the API server
 app.listen(PORT, function() {
